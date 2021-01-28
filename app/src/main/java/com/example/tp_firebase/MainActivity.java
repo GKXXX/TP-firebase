6be77f0e47733f6ba40ca04530ad7fa480cc0f65
GKXXX <killiangassin78@gmail.com>
2021-01-28 16:10:40 +0100

    Crea fragment ajout livre

--- app/src/main/java/com/example/tp_firebase/MainActivity.java
+++ app/src/main/java/com/example/tp_firebase/MainActivity.java
@@ -2,20 +2,40 @@ package com.example.tp_firebase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button boutonAjoutLivre;
+    private class listenerBouton implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.boutonAjoutLivre:
+                    lanceActiviteAjoutLivre();
+            }
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listenerBouton listenerBouton=new listenerBouton();
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("livre/");
-        myRef.child("1/auteur").setValue("Molière");
+        boutonAjoutLivre = findViewById(R.id.boutonAjoutLivre);
+        boutonAjoutLivre.setOnClickListener(listenerBouton);
+    }
+    public void lanceActiviteAjoutLivre(){
+        Intent intent = new Intent(MainActivity.this,AjoutLivreActivite.class);
+        startActivity(intent);
     }
 }
